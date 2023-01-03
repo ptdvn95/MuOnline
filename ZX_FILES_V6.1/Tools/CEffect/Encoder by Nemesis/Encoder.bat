98fdb8e5756b95c803f905352a7812b478bc6a00
ptdvn97 <ptdvn97@gmail.com>
2023-01-04 01:19:44 +0700

    Update server file

--- ZX_FILES_V6.1/Tools/CEffect/Encoder by Nemesis/Encoder.bat
+++ ZX_FILES_V6.1/Tools/CEffect/Encoder by Nemesis/Encoder.bat
@@ -7,4 +7,3 @@ Encoder.exe CMapRenderMesh.txt CMapRenderMesh.bmd
 Encoder.exe CMapEffect.txt CMapEffect.bmd
 Encoder.exe CPetEffect.txt CPetEffect.bmd
 Encoder.exe CPetRenderMesh.txt CPetRenderMesh.bmd
-Encoder.exe CEffectRenderMesh.txt CEffectRenderMesh.bmd
